c095cb7b251a0a47de2dd0114f7a77360c2f82bd
Connie Leighton <constancealeighton@gmail.com>
2018-09-02 20:05:03 +0100

    array problem

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
-const Munros = require('./models/munros.js');
-const ListView = require('./views/list_view.js');
-const SelectView = require('./views/select_view.js');
+const Pokemon = require('./models/pokemon.js');
+const PokemonSelectView = require('./views/select_view.js')
 
 document.addEventListener('DOMContentLoaded', () => {
-  const munros = new Munros ('https://munroapi.herokuapp.com/api/munros')
-  const container = document.querySelector('#container');
-  const listView = new ListView(container);
-  const dropdown = document.querySelector('#munro-dropdown');
-  const selectView = new SelectView(dropdown);
+  const pokemonSelectView = new PokemonSelectView;
+  pokemonSelectView.bindEvents();
 
-  selectView.bindEvents();
-  listView.bindEvents();
-  munros.getData();
-})
+  const pokemon = new Pokemon;
+  pokemon.bindEvents();
+  pokemon.getData();
+});
